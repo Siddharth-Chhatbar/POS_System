047b53859ee0f5600426327b9c56b18aa88b5fac
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2025-02-27 12:53:05 +0900

    routing and tailwind

--- pos/src/main.tsx
+++ pos/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
+import BillingPage from "./pages/BillingPage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}   />
+        <Route path="/billing" element={<BillingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
