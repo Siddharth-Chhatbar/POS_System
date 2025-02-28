bf0ccca36658878593e5163d83b06126515209f7
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2025-02-28 10:15:02 +0900

    pages setup

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -1,15 +1,60 @@
 import { NavLink } from "react-router";
 import "./App.css";
 
+/*
+ * Billing
+ * Inventory
+ * Suppliers
+ * Employees
+ * Report
+ * Federal Complient Reporting
+ * Backup Data
+ */
+interface PageOptions {
+  name: string;
+  path: string;
+}
+
 function App() {
+  const Pages: PageOptions[] = [
+    {
+      name: "Billing",
+      path: "/billing",
+    },
+    {
+      name: "Inventory",
+      path: "/inventory",
+    },
+    {
+      name: "Suppliers",
+      path: "/suppliers",
+    },
+    {
+      name: "Employees",
+      path: "/employees",
+    },
+    {
+      name: "Report",
+      path: "/report",
+    },
+    {
+      name: "Federal Complient Reporting",
+      path: "/federal-complient-reporting",
+    },
+    {
+      name: "Backup Data",
+      path: "/backup-data",
+    },
+  ];
   return (
-    <div className="">
-      <div className="text-2xl text-orange-300 font-bold">
-        Hello, World
-      </div>
-      <NavLink to="/billing" end>Billing</NavLink>
+    <div className="gap-2 flex flex-row pt-2 items-center justify-center font-semibold text-2xl">
+      {Pages.map((page: PageOptions) => (
+        <NavLink className="p-4 rounded bg-blue-800 text-white" to={page.path}>
+          {page.name}
+        </NavLink>
+      ))}
     </div>
-    );
+  );
 }
 
 export default App;
--- pos/src/main.tsx
+++ pos/src/main.tsx
@@ -3,13 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router";
 import BillingPage from "./pages/BillingPage";
+import InventoryPage from "./pages/InventoryPage";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}   />
+        <Route path="/" element={<App />} />
         <Route path="/billing" element={<BillingPage />}></Route>
+        <Route path="/inventory" element={<InventoryPage />}></Route>
+        <Route path="/suppliers" element={<BillingPage />}></Route>
+        <Route path="/employees" element={<BillingPage />}></Route>
+        <Route path="/report" element={<BillingPage />}></Route>
+        <Route
+          path="/federal-complient-reporting"
+          element={<BillingPage />}
+        ></Route>
+        <Route path="/backup-data" element={<BillingPage />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
